e09f46f2af1571718524b6f4d11ae2a7d8ba6bf8
Foyzul Karim <foyzulkarim@gmail.com>
2025-05-08 19:10:43 +1000

    chore: Update ESLint configuration to disable specific rules
    - Added eslint-disable comments for no-undef and @typescript-eslint/no-require-imports in eslint.config.js to improve linting flexibility.

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
 const js = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const eslintConfigPrettier = require('eslint-config-prettier');
